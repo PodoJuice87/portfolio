6e54487911c3530cb0011a984a89150f1217cb86
PodoJuice87 <79896012+PodoJuice87@users.noreply.github.com>
2022-05-31 12:35:08 +0900

    Delete color.js

--- color.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".color").each(function() {
-    var color = $(this).text();
-    $(this).css("color",color);
-    $(this).prepend("<div style='background:"+color+"'></div>");
-  });
-});
